47c675e0ec1c7bc2acd8d97d94bf3115f00d4462
hieu26102004 <136700894+hieu26102004@users.noreply.github.com>
2025-03-31 18:37:30 +0700

    Update search

--- app/src/main/java/com/phamhuong/library/TrendingBooksFragment.java
+++ app/src/main/java/com/phamhuong/library/TrendingBooksFragment.java
@@ -8,11 +8,39 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.phamhuong.library.adapter.BookAdapter;
+import com.phamhuong.library.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrendingBooksFragment extends Fragment {
+    private RecyclerView rvTrendingBooks;
+    private BookAdapter bookAdapter;
+    private List<Book> trendingBooks;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_trending_books, container, false);
+        View view = inflater.inflate(R.layout.fragment_trending_books, container, false);
+
+        // Ánh xạ RecyclerView
+        rvTrendingBooks = view.findViewById(R.id.rvTrendingBooks);
+        rvTrendingBooks.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        // Tạo danh sách sách thịnh hành
+        trendingBooks = new ArrayList<>();
+        trendingBooks.add(new Book());
+        trendingBooks.add(new Book());
+        trendingBooks.add(new Book());
+
+        // Gán adapter cho RecyclerView
+        bookAdapter = new BookAdapter(this.getContext(),trendingBooks);
+        rvTrendingBooks.setAdapter(bookAdapter);
+
+        return view;
     }
 }
