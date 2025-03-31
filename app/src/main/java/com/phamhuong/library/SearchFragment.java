48b3afbc67f7ef50ea6d028227490188e78fee79
hieu26102004 <136700894+hieu26102004@users.noreply.github.com>
2025-03-31 13:10:20 +0700

    Thêm tìm kiếm và lọc theo thể loại

--- app/src/main/java/com/phamhuong/library/SearchFragment.java
+++ app/src/main/java/com/phamhuong/library/SearchFragment.java
@@ -8,12 +8,28 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class SearchFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        // Load các fragment con vào container
+        loadFragment(new SearchInputFragment(), R.id.fragmentSearchInput);
+        loadFragment(new TrendingBooksFragment(), R.id.fragmentTrendingBooks);
+        loadFragment(new NewBooksFragment(), R.id.fragmentNewBooks);
+        loadFragment(new SearchResultsFragment(), R.id.fragmentSearchResults);
+
+        return view;
+    }
+    private void loadFragment(Fragment fragment, int containerId) {
+        FragmentManager fm = getChildFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(containerId, fragment);
+        ft.commit();
     }
 }
 
